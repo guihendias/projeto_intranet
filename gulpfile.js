3934d8f4cb5c81eb1b3d1fdd00978a201c61cffb
unknown <guilherme.dias@ATM08.intra.local>
2016-07-05 11:46:51 -0300

    pequenos ajustes

--- gulpfile.js
+++ gulpfile.js
@@ -6,26 +6,26 @@ var uglify = require('gulp-uglify');
 var es = require('event-stream');
 var htmlmin = require('gulp-htmlmin');
 var cleanCSS = require('gulp-clean-css');
-var runSequence = require('run-sequence')
-var rename = require('gulp-rename')
+var runSequence = require('run-sequence');
+var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 
 gulp.task('clean', function () {
-	return gulp.src('dist/app/css')
+	return gulp.src(['dist/app/css','dist/app/view','dist/app/js'])
 	.pipe(clean());
-})
+});
 
 gulp.task('sass', function () {
-	 
-	 gulp.src('src/app/scss/*.scss')  
+
+	 gulp.src('src/app/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
     	browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
 	}))
     .pipe(gulp.dest('src/app/css'));
-    
+
     gulp.src('src/app/css/style.css')
 	.pipe(cleanCSS())
 	.pipe(concat('styles.min.css'))
@@ -68,9 +68,9 @@ gulp.task('watch', function() {
       console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
   gulp.watch('src/app/view/*.html',['htmlmin']);
-  gulp.watch('src/app/**/*.js',['uglify'])
+  gulp.watch('src/app/**/*.js',['uglify']);
 });
 
 gulp.task('default', function (callback) {
-	return runSequence('clean',['htmlmin','sass','uglify','watch'],callback)
+	return runSequence('clean',['htmlmin','sass','uglify','watch'],callback);
 });
